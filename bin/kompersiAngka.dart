59827939d71c8f3674ea5f0bbda7a433181d5592
paozanwa22 <fausanwadi@gmail.com>
2021-06-26 14:25:10 +0800

    26/6/2021 Merubah Angka Ke Bentuk Huruf

--- bin/kompersiAngka.dart
+++ bin/kompersiAngka.dart
@@ -1,31 +1,32 @@
+
 void main(){
-  var angka = Konversi();
-  angka.angka(12);
+  Konversi a = Konversi();
+  var hasil = a.angka(1569);
+  print(hasil);
 }
 
-class  Konversi<String>{
-  String? angka(int satuan) {
-    List huruf = ["","Satu","Dua","Tiga","Empat","Lima","Enam","Tujuh","Delapan","Sebilan","Sepuluh","Sebelas"];
-    String hasil = "";
-    if (satuan < 12)
-      hasil = hasil + huruf[satuan];
-    else if (satuan < 20)
-      hasil = hasil + angka(satuan - 10) + " Belas";
-    else if (satuan < 100)
-      hasil = hasil + angka(satuan ~/ 10) + " Puluh " + angka(satuan % 10);
-    else if (satuan < 200)
-      hasil = hasil + "Seratus " + angka(satuan - 100);
-    else if (satuan < 1000)
-      hasil = hasil + angka(satuan ~/ 100) + " Ratus " + angka(satuan % 100);
-    else if (satuan < 2000)
-      hasil = hasil + "Seribu " + angka(satuan - 1000);
-    else if (satuan < 1000000)
-      hasil = hasil + angka(satuan ~/ 1000) + " Ribu " + angka(satuan % 1000);
-    else if (satuan < 1000000000)
-      hasil =
-          hasil + angka(satuan ~/ 1000000) + " Juta " + angka(satuan % 1000000);
-    else if (satuan >= 1000000000)
-      hasil = "Angka terlalu besar, harus kurang dari 1 milyar!";
-    print(hasil);
+class  Konversi{
+  String angka(int satuan){
+    var huruf =["","Satu","Dua","Tiga","Empat","Lima","Enam","Tujuh","Delapan","Sebilan","Sepuluh","Sebelas"];
+    String hasil="";
+    if(satuan<12)
+      hasil=hasil+huruf[satuan];
+    else if(satuan<20)
+      hasil=hasil+angka(satuan-10)+" Belas";
+    else if(satuan<100)
+      hasil=hasil+angka(satuan ~/10)+" Puluh "+angka(satuan%10);
+    else if(satuan<200)
+      hasil=hasil+"Seratus "+angka(satuan-100);
+    else if(satuan<1000)
+      hasil=hasil+angka(satuan~/100)+" Ratus "+angka(satuan%100);
+    else if(satuan<2000)
+      hasil=hasil+"Seribu "+angka(satuan-1000);
+    else if(satuan<1000000)
+      hasil=hasil+angka(satuan~/1000)+" Ribu "+angka(satuan%1000);
+    else if(satuan<1000000000)
+      hasil=hasil+angka(satuan~/1000000)+" Juta "+angka(satuan%1000000);
+    else if(satuan>=1000000000)
+      hasil="Angka terlalu besar, harus kurang dari 1 milyar!";
+    return hasil;
   }
 }
