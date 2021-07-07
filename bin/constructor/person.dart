fc0bcb9648f4b12e30915acfe1eaf0003c95c924
paozanwa22 <fausanwadi@gmail.com>
2021-07-07 11:31:17 +0800

    7/7/2021 constructor

---
+++ bin/constructor/person.dart
@@ -0,0 +1,21 @@
+class Person {
+  // 1
+  String? name;  //bisa di isi melalui main atau disini(constructor)
+  // CONSTRUCTOR
+    // Person(){ //ini akan dijalankan ketika objek dibuat
+      // name = 'no name'; //kita bisa mengisi proferti name
+      // print('ini adalah constructor');
+    // }
+
+    // 2
+    // parameter
+    // Person(String name){
+    //   this.name = name; //this.name menunjuk pada proferti class Person
+    // }  
+
+    // 4
+    // optional parameter
+    // Person({String name = 'no name'}){ 
+    //   this.name = name; 
+    // }
+}
---
+++ bin/constructor/student.dart
@@ -0,0 +1,12 @@
+import 'person.dart';
+
+class Student extends Person {
+
+  // 5
+  // Student() : super('studen baru'); //panggil konstructor yang ada parameternya
+
+  // 
+  //jika Student ingin memiliki parameter
+  // Student({String nameOptional = 'optional studen name'}) : super(name: nameOptional);
+
+}
