a440038d5a82b7d372a88f5bba3ad7d2f761de49
paozanwa22 <fausanwadi@gmail.com>
2021-07-10 18:17:26 +0800

    10/7/2021 CRUD

--- bin/qurban/apQurban.dart
+++ bin/qurban/apQurban.dart
@@ -4,20 +4,20 @@ import 'hewanQurban.dart';
 
 class ProgramHewanQurban {
   // Penyimpan list hewan qurban
-  List<HewanQurban> hewanQurban = [];
+  var hewanQurban = <HewanQurban>[];
   var runing = true;
 
   void showMenu() {
     print('');
     print('Menu');
-    print("1. daftarkan hewan Qurban");
-    print("2. lihat daftar hewan Qurban");
-    print("3. update data hewan qurban");
-    print("4. hapus data hewan qurban");
-    print("5. bersihkan data");
-    print("Pilih menu : \n");
+    print('1. daftarkan hewan Qurban');
+    print('2. lihat daftar hewan Qurban');
+    print('3. update data hewan qurban');
+    print('4. hapus data hewan qurban');
+    print('5. bersihkan data');
+    print('Pilih menu : \n');
     var menu = stdin.readLineSync()!;
-    selectedMenu(int.tryParse(menu) ?? 0);
+    selectedMenu(int.parse(menu));
   }
 
   void start() {
@@ -26,66 +26,67 @@ class ProgramHewanQurban {
     }
   }
 
-  // tidak perlu
-  void namaHewan() {
-    stdout.write('nama Hewan: ');
-    var nama = stdin.readLineSync()!;
-    print(nama);
-  }
-
-  void jumlah() {
-    stdout.write('nama Hewan: ');
-    var nama = stdin.readLineSync()!;
-    print(nama);
-  }
-
-  void bobot() {
-    stdout.write('nama Hewan: ');
-    var nama = stdin.readLineSync()!;
-    print(nama);
-  }
-
-  void donatur() {
-    stdout.write('nama Hewan: ');
-    var nama = stdin.readLineSync()!;
-    print(nama);
-  }
-  //
-
   void selectedMenu(int menu) {
     switch (menu) {
       case 1:
         // Fungsi input
-        HewanQurban _newHewan = HewanQurban()
-          ..create()
-          ..cetak();
+        var _newHewan = HewanQurban()
+          ..create();
         // Tampung hewan qurban yang ditambahkan ke list hewan qurban  
-        this.hewanQurban.add(_newHewan);
-        this.showMenu();
+        hewanQurban.add(_newHewan);
+        showMenu();
+
         break;
       case 2:
         // fungsi tampilkan list qurban
-        stdout.write("Daftar hewan qurban \n");
-        this.hewanQurban.forEach((hewan) {
-          hewan.cetak();
+        stdout.write('Daftar hewan qurban \n');
+        hewanQurban.forEach((hewan) {
+          print(hewan.cetak());
         });
-        this.showMenu();
+        showMenu();
 
         break;
       case 3:
         // ubah data
+        print('Daftar hewan Qurban');
+        for(var i = 0; i < hewanQurban.length; i++){
+          print('$i, ${hewanQurban[i].cetak()}');
+        }
 
-        this.showMenu();
+        print('\nUpdate data ke: \n');
+        var u = int.parse(stdin.readLineSync()!);
+        for(var i = 0; i < hewanQurban.length; i++){
+          if( u == i ){
+            print('$i, ' + hewanQurban[i].cetak());
+            hewanQurban[i].runUpdate();
+          }
+        }
+
+        showMenu();
         break;
       case 4:
-        //  hapus data`
-        this.showMenu();
+        //  hapus data
+        print('Daftar hewan Qurban');
+        for(var i = 0; i < hewanQurban.length; i++){
+          print('$i, ${hewanQurban[i].cetak()}');
+        }
+
+        print('\nHapus data ke: \n');
+        var u = int.parse(stdin.readLineSync()!);
+        for(var i = 0; i < hewanQurban.length; i++){
+          if( u == i ){
+            print('$i, ' + hewanQurban[i].cetak());
+            hewanQurban.removeAt(i);
+          }
+        }
+
+        showMenu();
         break;
       case 5: 
-        this.hewanQurban.clear();
+        hewanQurban.clear();
         break;
       default:
-        this.runing = false;
+        runing = false;
         break;
     }
   }
--- bin/qurban/hewanQurban.dart
+++ bin/qurban/hewanQurban.dart
@@ -1,6 +1,6 @@
   import 'dart:io';
 
-  List<String> jenisHewanQurban = ["sapi", "onta", "kambing"];
+  List<String> jenisHewanQurban = ['sapi', 'onta', 'kambing'];
 
   class HewanQurban {
     String? namaHewan;
@@ -8,62 +8,109 @@
     int? bobot;
     List<String> donatur = [];
 
+    bool updateRuning = false;
+
     void create() {
-      this.setNamaHewan();
-      this.setJumlah();
-      this.setBobot();
-      this.setDonatur();
+      sNamaHewan();
+      sJumlah();
+      sBobot();
+      sDonatur();
     }
 
-    void setNamaHewan() {
-      stdout.write("Masukkan nama hewan : ");
+    void updateMenu(){
+      print('');
+      print('Menu');
+      print('1. ubah nama');
+      print('2. ubah jumlah');
+      print('3. ubah bobot');
+      print('4. ubah donatur');
+      print('Pilih menu : \n');
+      var menu = stdin.readLineSync()!;
+      update(int.parse(menu));
+    }
+
+    void runUpdate(){
+      updateRuning = true;
+      while(updateRuning){
+        updateMenu();
+
+      }
+    }
+
+    void update(int menu) {
+      switch(menu){
+        case 1:
+          sNamaHewan();
+          updateMenu();
+        break;
+        case 2:
+          sJumlah();
+          updateMenu();
+        break;
+        case 3:
+          sBobot();
+          updateMenu();
+        break;
+        case 4:
+          sDonatur();
+          updateMenu();
+        break;
+        default:
+          updateRuning = false;
+        break;
+      }
+    }
+
+    void sNamaHewan() {
+      stdout.write('Masukkan nama hewan : ');
       var _namaHewan = stdin.readLineSync()!;
       if (jenisHewanQurban.contains(_namaHewan.toLowerCase())){
-        this.namaHewan = _namaHewan;
+        namaHewan = _namaHewan;
       }else{
-        print("Hewan yang anda masukkan, tidak termasuk hewan qurban");
+        print('Hewan yang anda masukkan, tidak termasuk hewan qurban');
       }
     }
 
-    void setJumlah() {
-      stdout.write("Masukkan jumlah hewan : ");
-      this.jumlah = int.tryParse(stdin.readLineSync()!);
+    void sJumlah() {
+      stdout.write('Masukkan jumlah hewan : ');
+      jumlah = int.tryParse(stdin.readLineSync()!);
     }
 
-    void setBobot() {
-      stdout.write("Masukkan bobot hewan : ");
-      this.bobot = int.tryParse(stdin.readLineSync()!);
+    void sBobot() {
+      stdout.write('Masukkan bobot hewan : ');
+      bobot = int.tryParse(stdin.readLineSync()!);
     }
 
-    void setDonatur() {
-      stdout.write("Masukkan jumlah donatur : ");
-      var _jumlahDonatiur = int.tryParse(stdin.readLineSync()??"")??0;
-      for (int i = 0 ; i<_jumlahDonatiur;i++) {
-      stdout.write("Masukkan nama donatur ke-${i+1} : ");
-        this.donatur.add(stdin.readLineSync()??'');
+    void sDonatur() {
+      stdout.write('Masukkan jumlah donatur : ');
+      var _jumlahDonatiur = int.tryParse(stdin.readLineSync()??'')??0;
+      for (var i = 0 ; i<_jumlahDonatiur;i++) {
+      stdout.write('Masukkan nama donatur ke-${i+1} : ');
+        donatur.add(stdin.readLineSync()??'');
       }
     }
 
-    void cetak() {
-        print("${this.namaHewan}, ${this.jumlah}, ${this.bobot}, ${this.donatur.toString().replaceAll("[", "").replaceAll("]", "")}");
+    String cetak() {
+        return '$namaHewan, $jumlah, $bobot, ${donatur.toString().replaceAll('[', '').replaceAll(']', '')}';
+
     }
   }
 
   void main(){
-    List<HewanQurban> _listHewanQurban = [];
+    var _listHewanQurban = <HewanQurban>[];
 
-    stdout.write("Berapa kelompok ? ");
-    int jpk = int.parse(stdin.readLineSync()!);
-    for (int i = 0; i < jpk; i++) {
-      stdout.write("\nDaftarkan hewan qurban anda \n");
-      HewanQurban _hewanQurban = new HewanQurban();
-      _hewanQurban.setNamaHewan();
-      _hewanQurban.setJumlah();
-      _hewanQurban.setBobot();
-      _hewanQurban.setDonatur();
+    stdout.write('Berapa kelompok ? ');
+    var jpk = int.parse(stdin.readLineSync()!);
+    for (var i = 0; i < jpk; i++) {
+      stdout.write('\nDaftarkan hewan qurban anda \n');
+      var _hewanQurban = HewanQurban()
+      ..sNamaHewan()
+      ..sJumlah()
+      ..sBobot()
+      ..sDonatur();
       _listHewanQurban.add(_hewanQurban);
     }
-      _listHewanQurban.forEach((_hewan) {
-    _hewan.cetak();
-      });
+    _listHewanQurban.forEach((_hewan) {
+      _hewan.cetak();
+    });
   }
